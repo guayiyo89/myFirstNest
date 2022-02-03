b2b746642e3bd99a5f21d77cf769e8566fe09a61
guayiyo89 <guayiyo89@gmail.com>
2022-02-03 12:33:48 -0300

    jwToken fix routes

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Body, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { Controller, Body, Get, Post, Put, Delete, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateNewsDto } from './dto/create-news.dto';
 import { UpdateNewsDto } from './dto/update-news.dto';
 import { NewsService } from './news.service';
@@ -7,11 +8,13 @@ import { NewsService } from './news.service';
 export class NewsController {
     constructor(private readonly newsService: NewsService) { }
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     async index(){
         return await this.newsService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
     async find(@Param('id') _id:string){
         return await this.newsService.findOne(_id);
@@ -27,6 +30,7 @@ export class NewsController {
         return await this.newsService.update(_id, updateNewsDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     async delete(@Param('id') id:string){
         return await this.newsService.delete(id);
